dbc5aa2a5e987baf68ec0314b590e8cb49fb130e
ayt <y00913@naver.com>
2022-04-19 14:36:56 +0900

    db 연동 및 게시글 생성 수정 구현

---
+++ src/main/java/com/example/myhome/model/Board.java
@@ -0,0 +1,26 @@
+package com.example.myhome.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Board {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotNull
+    @Size(min=2, max=30, message = "제목은 2자이상 30자 이하입니다.")
+    private String title;
+    private String content;
+}
